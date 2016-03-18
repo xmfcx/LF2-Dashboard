d8b13c53ddf6e76c0294c875acfc6db7aedb16e1
MFC <xmfcx@outlook.com>
2016-03-18 14:04:03 +0200

    Beginning
    wow

---
+++ LF2Dashboard/Character.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LF2Dashboard
+{
+	class Character
+	{
+		public string Name;
+		public Bitmap Pic;
+		public int Address;
+
+		public Character(string name, Bitmap pic, int address)
+		{
+			Name = name;
+			Pic = pic;
+			Address = address;
+		}
+
+		public Character()
+		{
+		}
+	}
+}
